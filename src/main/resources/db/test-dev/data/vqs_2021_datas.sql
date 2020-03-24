9fe95f3e943c0de8744768b4640f826b4082655c
Samuel Corcaud <samuel.corcaud@keyconsulting.fr>
2020-03-24 17:21:11 +0000

    Merged PR 36: Init Data

---
+++ src/main/resources/db/test-dev/data/vqs_2021_datas.sql
@@ -0,0 +1,3 @@
+INSERT INTO data(id, version, value, reporting_unit_id) VALUES(20,'INIT', null,20);
+INSERT INTO data(id, version, value, reporting_unit_id) VALUES(21,'INIT', null,21);
+INSERT INTO data(id, version, value, reporting_unit_id) VALUES(22,'INIT', null,22);
