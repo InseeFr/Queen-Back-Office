11aed5e4fd040253dad371a7d3fa6ab3041c8219
Benjamin Claudel <benjamin.claudel@keyconsulting.fr>
2020-03-27 10:45:06 +0000

    Merged PR 40: feat: externalized properties
    feat: externalized properties

--- README.md
+++ README.md
@@ -5,30 +5,82 @@ REST API for communication between QUEEN DB and QUEEN Form (Frontend).
 ## Requirements
 For building and running the application you need:
 - [JDK 1.11](https://jdk.java.net/archive/)
-- Maven 3
+- Maven 3  
 
 ## Install and excute unit tests
 Use the maven clean and maven install 
 ```shell
 mvn clean install
-```
+```  
 
 ## Running the application locally
-Use the [Spring Boot Maven plugin](https://docs.spring.io/spring-boot/docs/current/reference/html/build-tool-plugins-maven-plugin.html) like so:
+Use the [Spring Boot Maven plugin](https://docs.spring.io/spring-boot/docs/current/reference/html/build-tool-plugins-maven-plugin.html) like so:  
 ```shell
 mvn spring-boot:run
-```
+```  
 
-## Application Accesses
+## Application Accesses locally
 To access to swagger-ui, use this url : [http://localhost:8080/api/swagger-ui.html](http://localhost:8080/api/swagger-ui.html)  
-To access to h2 console, use this url : [http://localhost:8080/api/h2-console](http://localhost:8080/api/h2-console)
+To access to h2 console, use this url : [http://localhost:8080/api/h2-console](http://localhost:8080/api/h2-console)  
+
+
+## Deploy application on Tomcat server
+### 1. Package the application
+Use the [Spring Boot Maven plugin]  (https://docs.spring.io/spring-boot/docs/current/reference/html/build-tool-plugins-maven-plugin.html) like so:  
+```shell
+mvn clean package
+```  
+The war will be generate in `/target` repository  
+
+### 2. Install tomcat and deploy war
+To deploy the war file in Tomcat, you need to : 
+Download Apache Tomcat and unpackage it into a tomcat folder  
+Copy your WAR file from target/ to the tomcat/webapps/ folder  
+
+### 3. Tomcat config
+Before to startup the tomcat server, some configurations are needed : 
+ 
+#### Define the external properties path
+In your tomcat/conf folder, open catalina.properties file and add in shared.loader the classpath to your properties folder  
+Exemple :  
+```shell
+shared.loader=D:/config
+```  
+#### Define the profile used
+In your tomcat folder, open create setenv.sh file with the following command :  
+```shell
+JAVA_OPTS="$JAVA_OPTS -Dspring.profiles.active=prod,insee"
+```  
+
+#### Properties file
+In the classpath define before, create application-insee.properties and complete the folowing properties:  
+```shell  
+database.url=jdbc:postgresql://your/path/to/DB  
+database.username=username  
+database.password=password  
+  
+defaultSchema=schema  
+application.mode=Basic #define the authentication mode (Basic, Keycloak or NoAuth)  
+```  
+
+### 4. Tomcat start
+From a terminal navigate to tomcat/bin folder and execute  
+```shell
+catalina.bat run (on Windows)
+```  
+```shell
+catalina.sh run (on Unix-based systems)
+```  
+
+### 5. Application Access
+To access to swagger-ui, use this url : [http://localhost:8080/queen-0.0.1-SNAPSHOT/swagger-ui.html](http://localhost:8080/queen-0.0.1-SNAPSHOT/swagger-ui.html)  
 
 ## Before you commit
-Before committing code please ensure,
-1 - README.md is updated
-2 - A successful build is run and all tests are sucessful
-3 - All newly implemented APIs are documented
-4 - All newly added properties are documented 
+Before committing code please ensure,  
+1 - README.md is updated  
+2 - A successful build is run and all tests are sucessful  
+3 - All newly implemented APIs are documented  
+4 - All newly added properties are documented  
 
 ## End-Points
 - Operation
--- src/main/java/fr/insee/queen/api/configuration/SecurityConfiguration.java
+++ src/main/java/fr/insee/queen/api/configuration/SecurityConfiguration.java
@@ -17,107 +17,129 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.savedrequest.NullRequestCache;
 
 /**
-* SecurityConfiguration is the class using to configure security.<br>
-* 3 ways to authenticated : <br>
-* 	0 - without authentication,<br>
-* 	1 - basic authentication <br>
-* 	2 - and keycloak authentication <br>
-* 
-* @author Claudel Benjamin
-* 
-*/
+ * SecurityConfiguration is the class using to configure security.<br>
+ * 3 ways to authenticated : <br>
+ * 0 - without authentication,<br>
+ * 1 - basic authentication <br>
+ * 2 - and keycloak authentication <br>
+ * 
+ * @author Claudel Benjamin
+ * 
+ */
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	/**
-	 * The environment define in Spring application
-	 * Generate with the application property environment
+	 * The environment define in Spring application Generate with the application
+	 * property environment
 	 */
 	@Autowired
 	private Environment environment;
-	
+
 	@Autowired
 	private ApplicationProperties applicationProperties;
 
-    /**
-     * This method check if environment is development or test
-     * @return true if environment matchs 
-     */
+	/**
+	 * This method check if environment is development or test
+	 * 
+	 * @return true if environment matchs
+	 */
 	protected boolean isDevelopment() {
-		return ArrayUtils.contains(environment.getActiveProfiles(), "dev") || ArrayUtils.contains(environment.getActiveProfiles(), "test");
+		return ArrayUtils.contains(environment.getActiveProfiles(), "dev")
+				|| ArrayUtils.contains(environment.getActiveProfiles(), "test");
 	}
-	
-    /**
-     * This method configure the WEB security access
-     */
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring()
-            .antMatchers(HttpMethod.OPTIONS, "/**")
-            .antMatchers("/i18n/**")
-            .antMatchers("/content/**")
-            .antMatchers("/test/**")
-            .antMatchers("/h2-console/**")
-            .antMatchers("/v2/**")
-            .antMatchers("/swagger-resources/**")
-            .antMatchers("/webjars/**")
-            .antMatchers("/swagger-ui.html")
-        	.antMatchers("/actuator/**")
-            .antMatchers("/login.html")
-            .antMatchers("/login")
-        	.antMatchers("/");
-    }
 
-    /**
-     * This method configure the HTTP security access
-     */
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-	    http
-	    	.csrf()
-	        .disable()
-	        .headers()
-	        .frameOptions()
-	        .disable()
-	    .and()
-	    	.requestCache()
-			.requestCache(new NullRequestCache());
-        if (isDevelopment()) 
-        {
-	    	http.httpBasic().authenticationEntryPoint(unauthorizedEntryPoint());
-	    }else {
-	    	if(ApplicationProperties.Mode.Basic == this.applicationProperties.getMode()) {
-	    		http.httpBasic().authenticationEntryPoint(unauthorizedEntryPoint());
-	    	}
-	    }
-        
-		http.authorizeRequests()
-        .antMatchers("/operations").hasRole("enqueteur")
-        .antMatchers("/operation/{idOperation}/reporting-units").hasRole("enqueteur")
-        .antMatchers("/operation/{idOperation}/questionnaire").hasRole("enqueteur")
-        .antMatchers("/operation/{id}/required-nomenclatures").hasRole("enqueteur")  
-        .antMatchers("/reporting-unit/{id}/data").hasRole("enqueteur")
-        .antMatchers("/reporting-unit/{id}/comment").hasRole("enqueteur")
-        .antMatchers("/nomenclature/{id}").hasRole("enqueteur")
-        .anyRequest().denyAll();
-    }
-    
-    /**
-     * This method configure the authentication manager for DEV and TEST accesses
-     */
+	/**
+	 * This method configure the WEB security access
+	 */
 	@Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		if (isDevelopment()) 
-        {
-				auth.inMemoryAuthentication()
-				.withUser("admin").password("{noop}a").roles("enqueteur").and()
-				.withUser("noWrite").password("{noop}a").roles();
-        }
-    }
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring()
+			.antMatchers(HttpMethod.OPTIONS, "/**")
+			.antMatchers("/i18n/**")
+			.antMatchers("/content/**")
+			.antMatchers("/test/**")
+			.antMatchers("/h2-console/**")
+			.antMatchers("/v2/**")
+			.antMatchers("/swagger-resources/**")
+			.antMatchers("/webjars/**")
+			.antMatchers("/swagger-ui.html")
+			.antMatchers("/actuator/**")
+			.antMatchers("/login.html")
+			.antMatchers("/login")
+			.antMatchers("/");
+	}
 
-    /**
-     * This method configure the unauthorized accesses
-     */
+	/**
+	 * This method configure the HTTP security access
+	 */
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().headers().frameOptions().disable().and().requestCache()
+				.requestCache(new NullRequestCache());
+		if (isDevelopment()) {
+			switch (this.applicationProperties.getMode()) {
+			case Basic:
+				System.out.println(ApplicationProperties.Mode.Basic);
+				http.httpBasic().authenticationEntryPoint(unauthorizedEntryPoint());
+				http.authorizeRequests().antMatchers("/operations").hasRole("enqueteur")
+					.antMatchers("/operation/{idOperation}/reporting-units").hasRole("enqueteur")
+					.antMatchers("/operation/{idOperation}/questionnaire").hasRole("enqueteur")
+					.antMatchers("/operation/{id}/required-nomenclatures").hasRole("enqueteur")
+					.antMatchers("/reporting-unit/{id}/data").hasRole("enqueteur")
+					.antMatchers("/reporting-unit/{id}/comment").hasRole("enqueteur")
+					.antMatchers("/nomenclature/{id}").hasRole("enqueteur")
+					.anyRequest().denyAll(); 
+				break;
+			case Keycloak:
+				System.out.println(ApplicationProperties.Mode.Keycloak);
+				break;
+			default:
+				System.out.println(ApplicationProperties.Mode.NoAuth);
+				http.httpBasic().disable();
+				http.authorizeRequests()
+				.antMatchers("/operations",
+						"/operation/{idOperation}/reporting-units",
+						"/operation/{idOperation}/questionnaire",
+						"/operation/{id}/required-nomenclatures",
+						"/reporting-unit/{id}/data",
+						"/reporting-unit/{id}/comment",
+						"/nomenclature/{id}").permitAll();
+				break;
+			}
+			
+		} else {
+			switch (this.applicationProperties.getMode()) {
+			case Basic:
+				http.httpBasic().authenticationEntryPoint(unauthorizedEntryPoint());
+				break;
+			case Keycloak:
+				System.out.println(ApplicationProperties.Mode.Keycloak);
+				break;
+			default:
+				System.out.println(ApplicationProperties.Mode.NoAuth);
+				break;
+			}
+		}
+
+	}
+
+	/**
+	 * This method configure the authentication manager for DEV and TEST accesses
+	 */
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		if (isDevelopment()) {
+			auth.inMemoryAuthentication().withUser("admin").password("{noop}a").roles("enqueteur").and()
+					.withUser("noWrite").password("{noop}a").roles();
+		}else {
+			
+		}
+	}
+
+	/**
+	 * This method configure the unauthorized accesses
+	 */
 	public AuthenticationEntryPoint unauthorizedEntryPoint() {
 		return (request, response, authException) -> {
 			response.addHeader("WWW-Authenticate", "BasicCustom realm=\"MicroService\"");
