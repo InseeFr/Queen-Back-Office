e3884cc2c34f3221c1b17c3965606688dec31718
benoit <benoit.werquin@insee.fr>
2020-04-24 08:43:09 +0200

    Add comment on PR when success

---
+++ .ci/comment-pr.sh
@@ -0,0 +1,6 @@
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
+   curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
+   -d "{\"body\": \"Build Success, well done\"}" \
+   "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+fi
+
