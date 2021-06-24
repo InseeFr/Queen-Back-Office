c3ca3f90119991d31599df76001397a82892e3cb
Benjamin Claudel <benjamin.claudel@keyconsulting.fr>
2021-06-24 15:56:48 +0200

    feat : limit access to swagger in prod profile

--- src/main/java/fr/insee/queen/api/controller/WebController.java
+++ src/main/java/fr/insee/queen/api/controller/WebController.java
@@ -1,13 +1,12 @@
 package fr.insee.queen.api.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebController {
 	
-	 @RequestMapping(value = "/", method = RequestMethod.GET)
+	 @GetMapping(value = "/")
 	   public String redirect() {
 	      return "redirect:/swagger-ui.html";
 	   }
