7d7f17504d9c40886c7d9e7ebda6036ace92e09b
Benoit Werquin <benoit.werquin@insee.fr>
2021-12-22 10:44:25 +0100

    disable cache for profil test

--- src/main/java/fr/insee/queen/api/configuration/CacheConfig.java
+++ src/main/java/fr/insee/queen/api/configuration/CacheConfig.java
@@ -2,8 +2,10 @@ package fr.insee.queen.api.configuration;
 
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 @EnableCaching
+@Profile("!test")
 public class CacheConfig {
 }
