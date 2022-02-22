af8e505517dea2984fb871c773ae05997bec8cad
Simon Demazière <simon.demaziere@insee.fr>
2022-02-22 14:02:20 +0100

    check if qm value is lazy fetched

--- src/main/java/fr/insee/queen/api/service/impl/CampaignServiceImpl.java
+++ src/main/java/fr/insee/queen/api/service/impl/CampaignServiceImpl.java
@@ -32,30 +32,31 @@ import fr.insee.queen.api.service.SurveyUnitService;
 @Service
 @Transactional
 public class CampaignServiceImpl extends AbstractService<Campaign, String> implements CampaignService {
-	
-    protected final CampaignRepository campaignRepository;
 
-    protected final QuestionnaireModelRepository questionnaireModelRepository;
-    
-    protected final MetadataRepository metadataRepository;
-    
-    @Autowired
+	protected final CampaignRepository campaignRepository;
+
+	protected final QuestionnaireModelRepository questionnaireModelRepository;
+
+	protected final MetadataRepository metadataRepository;
+
+	@Autowired
 	private QuestionnaireModelService questionnaireModelService;
 
-    @Autowired
-    private SurveyUnitService surveyUnitService;    
-    
-    @Autowired
-    public CampaignServiceImpl(CampaignRepository campaignRepository, MetadataRepository metadataRepository, QuestionnaireModelRepository questionnaireModelRepository) {
-        this.campaignRepository = campaignRepository;
-        this.questionnaireModelRepository = questionnaireModelRepository;
-        this.metadataRepository = metadataRepository;
-    }
+	@Autowired
+	private SurveyUnitService surveyUnitService;
 
-    @Override
-    protected JpaRepository<Campaign, String> getRepository() {
-        return campaignRepository;
-    }
+	@Autowired
+	public CampaignServiceImpl(CampaignRepository campaignRepository, MetadataRepository metadataRepository,
+			QuestionnaireModelRepository questionnaireModelRepository) {
+		this.campaignRepository = campaignRepository;
+		this.questionnaireModelRepository = questionnaireModelRepository;
+		this.metadataRepository = metadataRepository;
+	}
+
+	@Override
+	protected JpaRepository<Campaign, String> getRepository() {
+		return campaignRepository;
+	}
 
 	@Override
 	public List<CampaignDto> findDtoBy() {
@@ -76,58 +77,60 @@ public class CampaignServiceImpl extends AbstractService<Campaign, String> imple
 	public void save(Campaign c) {
 		campaignRepository.save(c);
 	}
-	
+
 	@Override
 	public void saveDto(CampaignDto c) {
 		Set<QuestionnaireModel> qm = new HashSet<>();
-		
+
 		c.getQuestionnaireIds().stream().forEach(
-			id -> {
-				Optional<QuestionnaireModel> qmTemp = questionnaireModelRepository.findById(id);
-				if(questionnaireModelRepository.findById(id).isPresent()) {
-					qm.add(qmTemp.get());
-		}});
+				id -> {
+					Optional<QuestionnaireModel> qmTemp = questionnaireModelRepository.findById(id);
+					if (questionnaireModelRepository.findById(id).isPresent()) {
+						qm.add(qmTemp.get());
+					}
+				});
 		Campaign campaign = new Campaign(c.getId().toUpperCase(), c.getLabel(), qm);
 		campaign.setQuestionnaireModels(qm);
 		qm.parallelStream().forEach(q -> q.setCampaign(campaign));
 		campaignRepository.save(campaign);
 		questionnaireModelRepository.saveAll(qm);
-		//Add Metadata
-		if (c.getMetadata()!=null) {
+		// Add Metadata
+		if (c.getMetadata() != null) {
 			Metadata m = new Metadata(UUID.randomUUID(), c.getMetadata().getValue(), campaign);
 			m = metadataRepository.save(m);
 			campaign.setMetadata(m);
 			campaignRepository.save(campaign);
 		}
-		
+
 		// For mongoDB impl
 		Set<QuestionnaireModel> qms = campaign.getQuestionnaireModels();
-		 if(qms.isEmpty() && !qm.isEmpty()) {
-			 qms.addAll(qm);
-			 campaignRepository.save(campaign);
-		 }
+		if (qms.isEmpty() && !qm.isEmpty()) {
+			qms.addAll(qm);
+			campaignRepository.save(campaign);
+		}
 	}
-	
+
 	public Boolean checkIfQuestionnaireOfCampaignExists(CampaignDto campaign) {
-		return campaign.getQuestionnaireIds().stream().noneMatch(questionaire -> 
-			!questionnaireModelRepository.findById(questionaire).isPresent()
-			|| questionnaireModelRepository.findById(questionaire).get().getCampaign() != null
-		);
+		return campaign.getQuestionnaireIds().stream()
+				.noneMatch(questionaire -> !questionnaireModelRepository.findById(questionaire).isPresent()
+						|| questionnaireModelRepository.findById(questionaire).get().getCampaign() != null);
 	}
-	
+
 	public List<CampaignResponseDto> getAllCampaigns() {
 		List<Campaign> campaigns = campaignRepository.findAll();
 		return campaigns.stream()
 				.map(camp -> new CampaignResponseDto(
-					camp.getId(), 
-					camp.getQuestionnaireModels().stream()
-						.map(QuestionnaireModel::getId)
-						.collect(Collectors.toList())
-					)
-				)
+						camp.getId(),
+						camp.getQuestionnaireModels().stream()
+								.map(qm -> {
+									System.out.println(qm.getValue().asText());
+									return qm.getId();
+								})
+
+								.collect(Collectors.toList())))
 				.collect(Collectors.toList());
 	}
-	
+
 	public List<QuestionnaireIdDto> getQuestionnaireIds(String id) throws NotFoundException {
 		Optional<Campaign> campaignOptional = campaignRepository.findById(id);
 		if (campaignOptional.isPresent()) {
@@ -138,7 +141,7 @@ public class CampaignServiceImpl extends AbstractService<Campaign, String> imple
 			throw new NotFoundException("Campaign " + id + "not found in database");
 		}
 	}
-	
+
 	public List<QuestionnaireModelDto> getQuestionnaireModels(String id) throws NotFoundException {
 		Optional<Campaign> campaignOptional = campaignRepository.findById(id);
 		if (campaignOptional.isPresent()) {
@@ -149,13 +152,13 @@ public class CampaignServiceImpl extends AbstractService<Campaign, String> imple
 			throw new NotFoundException("Campaign " + id + "not found in database");
 		}
 	}
-	
+
 	@Override
 	public void delete(Campaign c) {
 		surveyUnitService.findByCampaignId(c.getId()).stream().forEach(su -> surveyUnitService.delete(su));
 		List<QuestionnaireModel> qmList = questionnaireModelService.findQuestionnaireModelByCampaignId(c.getId());
-		if(qmList!=null && !qmList.isEmpty())
-		qmList.stream().forEach(qm -> questionnaireModelRepository.delete(qm));
+		if (qmList != null && !qmList.isEmpty())
+			qmList.stream().forEach(qm -> questionnaireModelRepository.delete(qm));
 		campaignRepository.delete(c);
 	}
 }
