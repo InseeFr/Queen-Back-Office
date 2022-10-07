c2d205d0217bad4d9b30537e85098f124d8b7d8b
Eric Thuaud <eric.thuaud@insee.fr>
2022-10-07 15:53:05 +0200

    log4j improvement with path, method, user every line - add loginterceptor

---
+++ src/main/java/fr/insee/queen/api/configuration/LogInterceptor.java
@@ -0,0 +1,88 @@
+package fr.insee.queen.api.configuration;
+
+import fr.insee.queen.api.service.UtilsService;
+import org.apache.logging.log4j.ThreadContext;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+
+@Component
+public class LogInterceptor implements HandlerInterceptor {
+
+    private static final Logger logger = LoggerFactory.getLogger(LogInterceptor.class);
+   // @Value("${server.contextPath}")
+    // private String contextPath;
+
+    @Autowired
+    ApplicationProperties applicationProperties;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        long executionStartTime = System.currentTimeMillis();
+        request.setAttribute("start-time", executionStartTime);
+        String fishTag = UUID.randomUUID().toString();
+        String method = request.getMethod();
+        String operationPath = request.getRequestURI();
+
+        String userId = null;
+
+        switch (applicationProperties.getMode()) {
+            case basic:
+                Object basic = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+                if (basic instanceof UserDetails) {
+                    userId = ((UserDetails)basic).getUsername();
+                } else {
+                    userId = basic.toString();
+                }
+                break;
+            case keycloak:
+                KeycloakAuthenticationToken keycloak = (KeycloakAuthenticationToken) request.getUserPrincipal();
+                if(keycloak!=null) {
+                    userId = keycloak.getPrincipal().toString();
+                }
+                break;
+            default:
+                userId = "GUEST";
+                break;
+        }
+
+
+        ThreadContext.put("id", fishTag);
+        ThreadContext.put("path", operationPath);
+        ThreadContext.put("method", method);
+        ThreadContext.put("user", userId);
+
+        logger.info("["+userId+"] - ["+method+"] - ["+operationPath+"]");
+        return true;
+    }
+
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView mv) {
+        long executionStartTime = (Long) request.getAttribute("start-time");
+        long renderingStartTime = System.currentTimeMillis();
+        request.setAttribute("rendering-start-time", renderingStartTime);
+        long executionDuration = renderingStartTime - executionStartTime;
+        ThreadContext.put("execution-duration", String.valueOf(executionDuration));
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
+                                    Exception exception) throws Exception {
+        long renderingStartTime = (Long)request.getAttribute("rendering-start-time");
+        long renderingEndTime = System.currentTimeMillis();
+        long renderingDuration = renderingEndTime - renderingStartTime;
+        ThreadContext.put("rendering-duration", String.valueOf(renderingDuration));
+        ThreadContext.clearMap();
+    }
+}
