568f642a41f650ce21be0cf113e6dec0a945eab5
David Darras <david.darras@insee.fr>
2023-09-25 14:01:43 +0200

    fix: validate nomenclature input id

--- src/main/java/fr/insee/queen/api/dto/input/NomenclatureInputDto.java
+++ src/main/java/fr/insee/queen/api/dto/input/NomenclatureInputDto.java
@@ -2,12 +2,13 @@ package fr.insee.queen.api.dto.input;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.node.ArrayNode;
+import fr.insee.queen.api.controller.validation.IdValid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record NomenclatureInputDto(
-		@NotBlank
+		@IdValid
 		String id,
 		@NotBlank
 		String label,
