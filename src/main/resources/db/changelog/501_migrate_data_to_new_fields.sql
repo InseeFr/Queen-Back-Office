608da315391508ac826dc37e0f3a3cee364c8621
David Darras <david.darras@insee.fr>
2023-11-06 11:47:03 +0100

    fix: update idsu in paradata event

--- src/main/resources/db/changelog/501_migrate_data_to_new_fields.sql
+++ src/main/resources/db/changelog/501_migrate_data_to_new_fields.sql
@@ -9,3 +9,9 @@ where
             from survey_unit s
             where s.id=p.value->>'idSU'
     );
+
+--changeset davdarras:510-2
+update paradata_event
+set survey_unit_id = p2.value->>'idSU'
+from paradata_event p2
+where paradata_event.id = p2.id;
