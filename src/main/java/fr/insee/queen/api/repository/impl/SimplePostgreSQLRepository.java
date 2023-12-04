171e46f71d9644890f5243846fbefbcca742261f
Simon Demazière <simon.demaziere@insee.fr>
2023-12-04 15:32:33 +0100

    Develop (#175)
    * Fix datasetinjector (#147)
    
    * fix: dataset format
    
    * bump version to 3.5.43
    
    * update Personalization object => array
    
    * Collected -> Node of Node
    
    * feat: add endpoint for questionnaireModelId simple fetch (#148)
    
    * feat: add indexes for survey unit tables (#159)
    
    * Fix/prevent wrong duplicates (#165)
    
    * feat : check state-data date on SU update only
    
    * fix : liquibase xsd
    
    * fix: sonar compliance
    
    ---------
    
    Co-authored-by: Dylan Decrulle <dylan.decrulle@insee.fr>
    Co-authored-by: David Darras <117278497+davdarras@users.noreply.github.com>

--- src/main/java/fr/insee/queen/api/repository/impl/SimplePostgreSQLRepository.java
+++ src/main/java/fr/insee/queen/api/repository/impl/SimplePostgreSQLRepository.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Service;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -29,27 +30,27 @@ public class SimplePostgreSQLRepository implements SimpleApiRepository {
 
     @Override
     public void updateSurveyUnitData(String id, JsonNode data) {
-        updateJsonValueOfSurveyUnit("data",id,data);
+        updateJsonValueOfSurveyUnit("data", id, data);
     }
 
     @Override
-    public void updateSurveyUnitComment(String id, JsonNode comment)  {
-        updateJsonValueOfSurveyUnit("comment",id,comment);
+    public void updateSurveyUnitComment(String id, JsonNode comment) {
+        updateJsonValueOfSurveyUnit("comment", id, comment);
     }
 
     @Override
     public void updateSurveyUnitPersonalization(String id, JsonNode personalization) {
-        updateJsonValueOfSurveyUnit("personalization",id, personalization);
+        updateJsonValueOfSurveyUnit("personalization", id, personalization);
     }
 
-    public String getCampaignIdFromSuId(String id){
-        String qStringGetCampaignID= "SELECT campaign_id FROM survey_unit WHERE id=?";
+    public String getCampaignIdFromSuId(String id) {
+        String qStringGetCampaignID = "SELECT campaign_id FROM survey_unit WHERE id=?";
         return jdbcTemplate.queryForObject(
                 qStringGetCampaignID, new Object[] { id }, String.class);
     }
 
     @Override
-    public void updateSurveyUnitStateDate(String id, JsonNode stateData){
+    public void updateSurveyUnitStateDate(String id, JsonNode stateData) {
 
         String qStringGetSU = "SELECT count(*) FROM state_data WHERE survey_unit_id=?";
         Long date = stateData.get("date").longValue();
@@ -59,10 +60,10 @@ public class SimplePostgreSQLRepository implements SimpleApiRepository {
         int nbStateData = jdbcTemplate.queryForObject(
                 qStringGetSU, new Object[] { id }, Integer.class);
 
-        if(nbStateData>0) {
+        if (nbStateData > 0) {
             String qString = "UPDATE state_data SET current_page=?, date=?, state=? WHERE survey_unit_id=?";
             jdbcTemplate.update(qString, currentPage, date, state, id);
-        }else{
+        } else {
             LOGGER.info("INSERT state_data for reporting unit with id {}", id);
             String qString = "INSERT INTO state_data (id, current_page, date, state, survey_unit_id) VALUES (?, ?, ?, ?, ?)";
             jdbcTemplate.update(qString, UUID.randomUUID(), currentPage, date, state, id);
@@ -70,11 +71,9 @@ public class SimplePostgreSQLRepository implements SimpleApiRepository {
 
     }
 
-
-
     @Override
     public void createSurveyUnit(String campaignId, SurveyUnitResponseDto surveyUnitResponseDto) {
-        String su ="INSERT INTO survey_unit (id, campaign_id, questionnaire_model_id)\n" +
+        String su = "INSERT INTO survey_unit (id, campaign_id, questionnaire_model_id)\n" +
                 "VALUES (?,?,?)\n" +
                 "ON CONFLICT (id) DO UPDATE SET campaign_id=?, questionnaire_model_id=?";
         jdbcTemplate.update(su,
@@ -82,52 +81,52 @@ public class SimplePostgreSQLRepository implements SimpleApiRepository {
                 campaignId, surveyUnitResponseDto.getQuestionnaireId(),
                 campaignId, surveyUnitResponseDto.getQuestionnaireId());
 
-        insertJsonValueOfSurveyUnit("data",surveyUnitResponseDto.getId(),surveyUnitResponseDto.getData());
-        insertJsonValueOfSurveyUnit("comment",surveyUnitResponseDto.getId(),surveyUnitResponseDto.getComment());
-        insertJsonValueOfSurveyUnit("personalization",surveyUnitResponseDto.getId(),surveyUnitResponseDto.getPersonalization());
-        insertSurveyUnitStateDate(surveyUnitResponseDto.getId(),surveyUnitResponseDto.getStateData());
+        insertJsonValueOfSurveyUnit("data", surveyUnitResponseDto.getId(), surveyUnitResponseDto.getData());
+        insertJsonValueOfSurveyUnit("comment", surveyUnitResponseDto.getId(), surveyUnitResponseDto.getComment());
+        insertJsonValueOfSurveyUnit("personalization", surveyUnitResponseDto.getId(),
+                surveyUnitResponseDto.getPersonalization());
+        insertSurveyUnitStateDate(surveyUnitResponseDto.getId(), surveyUnitResponseDto.getStateData());
     }
 
     @Override
     public void deleteParadataEventsBySU(List<String> lstSU) {
-        String values = lstSU.stream().map(id->"?").collect(Collectors.joining(","));
+        String values = lstSU.stream().map(id -> "?").collect(Collectors.joining(","));
         StringBuilder qStringBuilder = new StringBuilder("DELETE FROM paradata_event AS paradataEvent ")
                 .append("WHERE paradataEvent.value ->> 'idSU' IN (%s)");
         String qString = String.format(qStringBuilder.toString(), values);
         jdbcTemplate.update(qString, lstSU.toArray());
     }
 
-    private void insertSurveyUnitStateDate(String surveyUnitId, StateDataDto stateData){
+    private void insertSurveyUnitStateDate(String surveyUnitId, StateDataDto stateData) {
         StateDataDto stateDataNpeProof = stateData != null ? stateData : new StateDataDto();
         Long date = stateDataNpeProof.getDate();
         String state = stateDataNpeProof.getState() != null ? stateDataNpeProof.getState().name() : null;
         String currentPage = stateDataNpeProof.getCurrentPage();
         String qString = "INSERT INTO state_data (id,current_page,date,state,survey_unit_id) VALUES (?,?,?,?,?)";
-        jdbcTemplate.update(qString,UUID.randomUUID(),currentPage,date,state,surveyUnitId);
+        jdbcTemplate.update(qString, UUID.randomUUID(), currentPage, date, state, surveyUnitId);
     }
 
-    private void insertJsonValueOfSurveyUnit(String table, String surveyUnitId, JsonNode jsonValue){
-        String qString = String.format("INSERT INTO %s (id, value, survey_unit_id) VALUES (?,?,?)",table);
+    private void insertJsonValueOfSurveyUnit(String table, String surveyUnitId, JsonNode jsonValue) {
+        String qString = String.format("INSERT INTO %s (id, value, survey_unit_id) VALUES (?,?,?)", table);
         PGobject json = new PGobject();
         json.setType("json");
         try {
             json.setValue(jsonValue.toString());
         } catch (SQLException throwables) {
-            LOGGER.error("Error when inserting in {} - {}",table,throwables.getMessage());
+            LOGGER.error("Error when inserting in {} - {}", table, throwables.getMessage());
             throwables.printStackTrace();
         }
-        jdbcTemplate.update(qString,UUID.randomUUID(),json,surveyUnitId);
+        jdbcTemplate.update(qString, UUID.randomUUID(), json, surveyUnitId);
     }
 
-
     private void updateJsonValueOfSurveyUnit(String table, String surveyUnitId, JsonNode jsonValue) {
-        String qString = String.format("UPDATE %s SET value=? WHERE survey_unit_id=?",table);
+        String qString = String.format("UPDATE %s SET value=? WHERE survey_unit_id=?", table);
         PGobject q = new PGobject();
         q.setType("json");
         try {
             q.setValue(jsonValue.toString());
         } catch (SQLException throwables) {
-            LOGGER.error("Error when updating in {} - {}",table,throwables.getMessage());
+            LOGGER.error("Error when updating in {} - {}", table, throwables.getMessage());
             throwables.printStackTrace();
         }
         jdbcTemplate.update(qString, q, surveyUnitId);
@@ -143,5 +142,18 @@ public class SimplePostgreSQLRepository implements SimpleApiRepository {
             return false;
         }
         return true;
+    }
+
+    @Override
+    public Optional<Long> findStateDataDateBySurveyUnitId(String id) {
+        String qStringGetStateDataWhereSurveyUnitId = "SELECT date FROM state_data WHERE survey_unit_id=?";
+        Long result;
+        try {
+            result = jdbcTemplate.queryForObject(
+                    qStringGetStateDataWhereSurveyUnitId, new Object[] { id }, Long.class);
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+        return Optional.of(result);
     }
 }
