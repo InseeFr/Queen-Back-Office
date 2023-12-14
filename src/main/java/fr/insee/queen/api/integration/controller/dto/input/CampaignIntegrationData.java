0d03209794f4749ba48217e897348acec3e41c19
David Darras <david.darras@insee.fr>
2023-12-14 17:36:03 +0100

    fix: handle campaign id uppercase & null metadata

--- src/main/java/fr/insee/queen/api/integration/controller/dto/input/CampaignIntegrationData.java
+++ src/main/java/fr/insee/queen/api/integration/controller/dto/input/CampaignIntegrationData.java
@@ -1,11 +1,11 @@
 package fr.insee.queen.api.integration.controller.dto.input;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import fr.insee.queen.api.campaign.service.model.Campaign;
 import fr.insee.queen.api.web.validation.IdValid;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record CampaignIntegrationData(
@@ -13,10 +13,13 @@ public record CampaignIntegrationData(
         String id,
         @NotBlank
         String label,
-        @NotNull
         ObjectNode metadata) {
 
     public static Campaign toModel(CampaignIntegrationData campaign) {
-        return new Campaign(campaign.id, campaign.label, campaign.metadata.toString());
+        ObjectNode metadata = campaign.metadata();
+        if(campaign.metadata() == null) {
+            metadata = JsonNodeFactory.instance.objectNode();
+        }
+        return new Campaign(campaign.id.toUpperCase(), campaign.label, metadata.toString());
     }
 }
