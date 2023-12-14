3484a52eb4d7eb2520b2f3c44f253a5663daf91d
David Darras <david.darras@insee.fr>
2023-12-14 17:40:03 +0100

    refactor: attribute names for nomanclatures/questionnaires

--- src/main/java/fr/insee/queen/api/integration/controller/component/builder/NomenclatureItem.java
+++ src/main/java/fr/insee/queen/api/integration/controller/component/builder/NomenclatureItem.java
@@ -3,7 +3,7 @@ package fr.insee.queen.api.integration.controller.component.builder;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import fr.insee.queen.api.campaign.service.model.Nomenclature;
 
-public record NomenclatureItem(String id, String label, String fileName) {
+public record NomenclatureItem(String id, String label, String filename) {
     public static Nomenclature toModel(NomenclatureItem nomenclatureItem, ArrayNode nomenclatureValue) {
         return new Nomenclature(nomenclatureItem.id, nomenclatureItem.label(), nomenclatureValue.toString());
     }
--- src/main/java/fr/insee/queen/api/integration/controller/component/builder/QuestionnaireModelItem.java
+++ src/main/java/fr/insee/queen/api/integration/controller/component/builder/QuestionnaireModelItem.java
@@ -1,6 +1,12 @@
 package fr.insee.queen.api.integration.controller.component.builder;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 
-public record QuestionnaireModelItem(String id, String label, String fileName, List<String> requiredNomenclatures) {
+public record QuestionnaireModelItem(String id,
+                                     String label,
+                                     String filename,
+                                     @JsonProperty("required-nomenclatures")
+                                     List<String> requiredNomenclatures) {
 }
