4c8e2f3c5caf369506f6e1f00a847c236ee1fbea
farid.ait-karra <farid.ait-karra@insee.fr>
2024-04-17 11:06:57 +0200

    feat/poc-protools : JMS integration - test 22

--- queen-listener-jms/src/main/java/fr/insee/queen/jms/configuration/NoOpCacheConfig.java
+++ queen-listener-jms/src/main/java/fr/insee/queen/jms/configuration/NoOpCacheConfig.java
@@ -1,16 +1,19 @@
 package fr.insee.queen.jms.configuration;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.support.NoOpCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+@Slf4j
 @Configuration
 public class NoOpCacheConfig {
     @Bean
     @Primary
     protected CacheManager cacheManager() {
+        log.info("NoOpCacheManager()");
         return new NoOpCacheManager();
     }
 }
