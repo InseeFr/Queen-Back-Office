94b72fabe811e33d3e24d518045c8d9ea214de46
davdarras <david.darras@insee.fr>
2024-06-06 16:56:19 +0200

    fix: add survey units unique constraints in DB

--- queen-domain/src/main/java/fr/insee/queen/domain/surveyunit/service/DataApiService.java
+++ queen-domain/src/main/java/fr/insee/queen/domain/surveyunit/service/DataApiService.java
@@ -6,6 +6,7 @@ import fr.insee.queen.domain.surveyunit.gateway.SurveyUnitRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Slf4j
@@ -21,11 +22,13 @@ public class DataApiService implements DataService {
     }
 
     @Override
+    @Transactional
     public void saveData(String surveyUnitId, ObjectNode dataValue) {
         surveyUnitRepository.saveData(surveyUnitId, dataValue);
     }
 
     @Override
+    @Transactional
     public void updateCollectedData(String surveyUnitId, ObjectNode collectedData) {
         surveyUnitRepository.updateCollectedData(surveyUnitId, collectedData);
     }
