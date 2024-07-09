f0f76e5bb705df8f62c6ba428c138fc443a39b28
farid.ait-karra <farid.ait-karra@insee.fr>
2024-07-09 14:47:54 +0200

    feat/poc-protools-pr : add JMS files

---
+++ queen-listener-jms/src/main/java/fr/insee/queen/jms/configuration/ConfigurationJMS.java
@@ -0,0 +1,43 @@
+package fr.insee.queen.jms.configuration;
+
+import jakarta.jms.ConnectionFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+@Slf4j
+@EnableJms
+@EnableScheduling
+@Configuration(enforceUniqueMethods = false)
+public class ConfigurationJMS {
+
+    public static final String UE_QUEUE = "queue-ue";
+    public static final String UE_REPLY_2_QUEUE = "reply-queue-ue";
+
+    @Bean
+    public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory,
+                                                    DefaultJmsListenerContainerFactoryConfigurer configurer) {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        // This provides all auto-configured defaults to this factory, including the message converter
+        configurer.configure(factory, connectionFactory);
+        // You could still override some settings if necessary.
+        return factory;
+    }
+
+    @Bean // Serialize message content to json using TextMessage
+    public MessageConverter jacksonJmsMessageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
