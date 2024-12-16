82e961f967c437efc0102f20f26ca1f43ccb3f6e
davdarras <david.darras@insee.fr>
2024-12-16 11:46:53 +0100

    refactor: avoid duplication codes for data repositories

--- queen-infra-db/src/main/java/fr/insee/queen/infrastructure/db/data/repository/jpa/DataJpaRepository.java
+++ queen-infra-db/src/main/java/fr/insee/queen/infrastructure/db/data/repository/jpa/DataJpaRepository.java
@@ -2,19 +2,54 @@ package fr.insee.queen.infrastructure.db.data.repository.jpa;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import fr.insee.queen.infrastructure.db.data.entity.common.DataDB;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
+import java.util.UUID;
 
-/**
- * JPA repository to handle survey unit's data response for a questionnaire
- */
-public interface DataJpaRepository {
+@NoRepositoryBean
+public interface DataJpaRepository extends JpaRepository<DataDB, UUID>, DataRepository {
     /**
      * Delete all survey units data for a campaign
      *
      * @param campaignId campaign id
      */
+    @Transactional
+    @Modifying
+    @Query(value = """
+            delete from data where survey_unit_id in (
+                select id from survey_unit
+                    where campaign_id = :campaignId
+            )""", nativeQuery = true)
     void deleteDatas(String campaignId);
+
+    /**
+     * Find the data of a survey unit
+     *
+     * @param surveyUnitId survey unit id
+     * @return an optional of the data (json format)
+     */
+    @Query(value = """
+            select s.data.value from SurveyUnitDB s
+            where s.id=:surveyUnitId
+       """)
+    Optional<ObjectNode> findData(String surveyUnitId);
+
+    /**
+     * Delete data of a survey unit
+     * @param surveyUnitId survey unit id
+     */
+    @Transactional
+    @Modifying
+    @Query(value =
+        """
+            delete from data where survey_unit_id = :surveyUnitId
+        """, nativeQuery = true)
+    void deleteBySurveyUnitId(String surveyUnitId);
 
     /**
      * Update data for a survey unit
@@ -23,35 +58,8 @@ public interface DataJpaRepository {
      * @param data json data to set
      * @return number of updated rows
      */
+    @Transactional
+    @Modifying
+    @Query("update DataDB d set d.value = :data where d.surveyUnit.id = :surveyUnitId")
     int updateData(String surveyUnitId, ObjectNode data);
-
-    /**
-     * Find the data of a survey unit
-     *
-     * @param surveyUnitId survey unit id
-     * @return an optional of the data (json format)
-     */
-    Optional<ObjectNode> findData(String surveyUnitId);
-
-    /**
-     * Delete data of a survey unit
-     * @param surveyUnitId survey unit id
-     */
-    void deleteBySurveyUnitId(String surveyUnitId);
-
-    /**
-     * Update data for a survey unit
-     *
-     * @param surveyUnitId survey unit id
-     * @param collectedUpdateData partial collected data to set on current collected data
-     */
-    void updateCollectedData(String surveyUnitId, ObjectNode collectedUpdateData);
-
-    /**
-     * Save data for a survey unit
-     *
-     * @param data data to save
-     * @return saved entity
-     */
-    DataDB save(DataDB data);
 }
