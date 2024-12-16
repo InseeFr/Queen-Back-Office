d3fd3f7f44c7e23e65f7b4f4424938df2c8d2737
davdarras <david.darras@insee.fr>
2024-12-16 10:58:26 +0100

    feat: forbid app loading when cipher enabled and empty key

--- queen-infra-db/src/main/java/fr/insee/queen/infrastructure/db/surveyunit/CipherProperties.java
+++ queen-infra-db/src/main/java/fr/insee/queen/infrastructure/db/surveyunit/CipherProperties.java
@@ -1,7 +1,20 @@
 package fr.insee.queen.infrastructure.db.surveyunit;
 
+import jakarta.validation.constraints.AssertTrue;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 @ConfigurationProperties(prefix = "feature.cipher")
-public record CipherProperties(boolean enabled) {
+@Validated
+public record CipherProperties(
+        boolean enabled,
+        String encryptionSecretKey) {
+
+    @AssertTrue(message = "encryptionSecretKey must not be blank when cipher is enabled")
+    public boolean isEncryptionSecretKeyValid() {
+        if (enabled) {
+            return encryptionSecretKey != null && !encryptionSecretKey.isBlank();
+        }
+        return true;
+    }
 }
