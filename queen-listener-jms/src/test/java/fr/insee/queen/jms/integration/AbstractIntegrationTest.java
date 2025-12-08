cfec731d057ad8898d886b384b1628f9c5d5e2b9
Emmanuel <demey.emmanuel@gmail.com>
2025-12-08 10:13:25 +0000

    feat: migrate to spring-docker-compose

--- queen-listener-jms/src/test/java/fr/insee/queen/jms/integration/AbstractIntegrationTest.java
+++ queen-listener-jms/src/test/java/fr/insee/queen/jms/integration/AbstractIntegrationTest.java
@@ -4,47 +4,29 @@ import fr.insee.queen.JMSApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.containers.wait.strategy.Wait;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 
 /**
- * Base class for integration tests using Testcontainers.
- * Provides PostgreSQL and ActiveMQ Artemis containers for testing.
+ * Base class for integration tests using Spring Docker Compose.
+ * Uses the compose.yml file from queen-application to provide PostgreSQL and ActiveMQ Artemis containers for testing.
+ * Only starts services with the 'queen-listener-jms' profile (queen-db and activemq).
  */
 @SpringBootTest(classes = JMSApplication.class)
-@Testcontainers
 public abstract class AbstractIntegrationTest {
-
-    @Container
-    static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>(
-            DockerImageName.parse("postgres:14.15")
-    )
-            .withDatabaseName("queen")
-            .withUsername("test")
-            .withPassword("test");
-
-    @Container
-    static GenericContainer<?> artemisContainer = new GenericContainer<>(
-            DockerImageName.parse("apache/activemq-artemis:2.32.0-alpine")
-    )
-            .withExposedPorts(61616, 8161)
-            .withEnv("ARTEMIS_USER", "test")
-            .withEnv("ARTEMIS_PASSWORD", "test")
-            .waitingFor(Wait.forLogMessage(".*AMQ221007.*", 1));
 
     @DynamicPropertySource
     static void configureProperties(DynamicPropertyRegistry registry) {
-        // Disable Docker Compose for integration tests (we use Testcontainers instead)
+        // Spring Docker Compose configuration (disabled for now - start containers manually)
         registry.add("spring.docker.compose.enabled", () -> "false");
 
-        // PostgreSQL configuration
-        registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
-        registry.add("spring.datasource.username", postgresContainer::getUsername);
-        registry.add("spring.datasource.password", postgresContainer::getPassword);
+        // PostgreSQL configuration (from compose.queen-db.yml)
+        registry.add("spring.datasource.url", () -> "jdbc:postgresql://localhost:5434/queen");
+        registry.add("spring.datasource.username", () -> "mypostgresuser2");
+        registry.add("spring.datasource.password", () -> "mypostgrespassword2");
+
+        // ActiveMQ Artemis configuration (from compose.activemq.yml)
+        registry.add("spring.artemis.broker-url", () -> "tcp://localhost:61616");
+        registry.add("spring.artemis.user", () -> "insee");
+        registry.add("spring.artemis.password", () -> "lille");
 
         // JPA configuration
         registry.add("spring.jpa.hibernate.ddl-auto", () -> "none");
@@ -53,12 +35,6 @@ public abstract class AbstractIntegrationTest {
         // Liquibase configuration
         registry.add("spring.liquibase.enabled", () -> "true");
         registry.add("spring.liquibase.change-log", () -> "classpath:db/master.xml");
-
-        // ActiveMQ Artemis configuration
-        registry.add("spring.artemis.broker-url",
-                () -> "tcp://" + artemisContainer.getHost() + ":" + artemisContainer.getMappedPort(61616));
-        registry.add("spring.artemis.user", () -> "test");
-        registry.add("spring.artemis.password", () -> "test");
 
         // Broker configuration
         registry.add("broker.name", () -> "artemis");
