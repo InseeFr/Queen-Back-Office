6c896cfd2124fbc71f78eaff4de0f3c7ff6ecc4d
davdarras <david.darras@insee.fr>
2026-03-04 15:47:53 +0100

    revert: init pulsar emitter consumer (#335)
    This reverts commit 8c31082f47357a2d52d4de1d5fb915bc186f0e6f and add the drop table for outbox

--- queen-domain/src/main/java/fr/insee/queen/domain/event/gateway/EventsRepository.java
+++
@@ -1,9 +0,0 @@
-package fr.insee.queen.domain.event.gateway;
-
-import com.fasterxml.jackson.databind.node.ObjectNode;
-
-import java.util.UUID;
-
-public interface EventsRepository {
-    void createEvent(UUID id, ObjectNode event);
-}
