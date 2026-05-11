309d7aa1a88275e200f110ff56f16baf7841af44
nsenave <nicolas.senave@gmail.com>
2026-05-11 17:47:25 +0200

    refactor(spring boot 4): now tests are compiling too

--- queen-application/src/test/java/fr/insee/queen/application/interrogation/integration/DataCipherIT.java
+++ queen-application/src/test/java/fr/insee/queen/application/interrogation/integration/DataCipherIT.java
@@ -9,7 +9,7 @@ import fr.insee.queen.infrastructure.db.data.repository.jpa.DataRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
--- queen-application/src/test/java/fr/insee/queen/application/interrogation/integration/InterrogationBatchCipherIT.java
+++ queen-application/src/test/java/fr/insee/queen/application/interrogation/integration/InterrogationBatchCipherIT.java
@@ -4,7 +4,7 @@ import fr.insee.queen.application.configuration.ScriptConstants;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
