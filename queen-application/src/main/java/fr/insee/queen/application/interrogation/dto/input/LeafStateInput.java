1eee3a33250970094ac332a48623645d78346f47
Laurent Caouissin <laurent.caouissin@insee.fr>
2026-09-18 16:37:37 +0200

    fix: stateData not persisted

---
+++ queen-application/src/main/java/fr/insee/queen/application/interrogation/dto/input/LeafStateInput.java
@@ -0,0 +1,4 @@
+package fr.insee.queen.application.interrogation.dto.input;
+
+public record LeafStateInput(String state, Long date) {
+}
--- queen-application/src/main/java/fr/insee/queen/application/interrogation/dto/input/StateDataInput.java
+++ queen-application/src/main/java/fr/insee/queen/application/interrogation/dto/input/StateDataInput.java
@@ -1,22 +1,34 @@
 package fr.insee.queen.application.interrogation.dto.input;
 
+import fr.insee.queen.domain.interrogation.model.LeafState;
 import fr.insee.queen.domain.interrogation.model.StateData;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+
+import java.util.List;
 
 @Schema(name = "StateDataUpdate")
 public record StateDataInput(
         @NotNull
         StateDataTypeInput state,
         @NotBlank
-        String currentPage) {
+        String currentPage,
+        List<LeafStateInput> leafStates) {
 
     public static StateData toModel(StateDataInput stateDataInputDto) {
         if (stateDataInputDto == null) {
             return null;
         }
-        return new StateData(stateDataInputDto.state().getStateDataType(), null, stateDataInputDto.currentPage);
+
+        if(stateDataInputDto.leafStates() == null) {
+            return new StateData(stateDataInputDto.state().getStateDataType(), null, stateDataInputDto.currentPage);
+        }
+        List<LeafState> leafStates = stateDataInputDto.leafStates()
+                .stream()
+                .map(leafStateInput -> new LeafState(leafStateInput.state(), leafStateInput.date()))
+                .toList();
+        return new StateData(stateDataInputDto.state().getStateDataType(), null, stateDataInputDto.currentPage, leafStates);
     }
 }
 
