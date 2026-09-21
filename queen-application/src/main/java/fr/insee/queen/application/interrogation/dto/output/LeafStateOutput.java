2b840730968c34eeab9cb76c3857b7d13d4f0511
Laurent Caouissin <laurent.caouissin@insee.fr>
2026-09-21 09:52:50 +0200

    fix: get leafState

---
+++ queen-application/src/main/java/fr/insee/queen/application/interrogation/dto/output/LeafStateOutput.java
@@ -0,0 +1,4 @@
+package fr.insee.queen.application.interrogation.dto.output;
+
+public record LeafStateOutput(String state, Long date) {
+}
--- queen-application/src/main/java/fr/insee/queen/application/interrogation/dto/output/StateDataDto.java
+++ queen-application/src/main/java/fr/insee/queen/application/interrogation/dto/output/StateDataDto.java
@@ -1,19 +1,33 @@
 package fr.insee.queen.application.interrogation.dto.output;
 
+import fr.insee.queen.domain.interrogation.model.LeafState;
 import fr.insee.queen.domain.interrogation.model.StateData;
 import fr.insee.queen.domain.interrogation.model.StateDataType;
 import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.util.List;
 
 @Schema(name = "StateData")
 public record StateDataDto(
         StateDataType state,
         Long date,
-        String currentPage) {
+        String currentPage,
+        List<LeafStateOutput> leafStates) {
     public static StateDataDto fromModel(StateData stateData) {
         if (stateData == null) {
             return null;
         }
-        return new StateDataDto(stateData.state(), stateData.date(), stateData.currentPage());
+
+        if(stateData.leafStates() == null){
+            return new StateDataDto(stateData.state(), stateData.date(), stateData.currentPage(), null);
+        }
+
+        List<LeafStateOutput> leafStates = stateData.leafStates()
+                .stream()
+                .map(leafState -> new LeafStateOutput(leafState.state(), leafState.date()))
+                .toList();
+        return new StateDataDto(stateData.state(), stateData.date(), stateData.currentPage(), leafStates);
+
     }
 }
 
